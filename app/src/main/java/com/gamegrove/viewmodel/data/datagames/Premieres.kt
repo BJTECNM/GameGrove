0d7c79c49b6bf02863531c4312e831f8ba635cb1
BJ <192t0220@itsm.edu.mx>
2023-12-15 15:40:35 -0600

    - Change/add code - Add resources

--- app/src/main/java/com/gamegrove/viewmodel/data/datagames/Premieres.kt
+++ app/src/main/java/com/gamegrove/viewmodel/data/datagames/Premieres.kt
@@ -5,6 +5,13 @@ import com.gamegrove.R
 object Premieres {
     val items = listOf(
         Game(
+            title = "Grand Theft Auto VI",
+            image = R.drawable.residentevil4remake,
+            launch = 2025,
+            plataform = "PC, Xbox Series X/S, PlayStation 5",
+            description = ""
+        ),
+        Game(
             title = "Prince Of Persia The Lost Crown",
             image = R.drawable.residentevil4remake,
             launch = 2024,
